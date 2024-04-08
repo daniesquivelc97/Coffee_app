777245790ce4c4ef15af18b2d2dbca33ad11aa84
daniel-esquivel-disney <Daniel.Esquivel.-ND@disney.com>
2024-04-08 13:30:43 -0500

    Update coffee_response_test.dart

--- test/infrastructure/models/coffee_response_test.dart
+++ test/infrastructure/models/coffee_response_test.dart
@@ -2,26 +2,28 @@ import 'package:coffee_app/infrastructure/models/coffee_response.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Create Coffee Response object', () {
-    final coffee = CoffeeResponse(file: 'coffee.jpg');
+  group('Coffee Response', () {
+    test('Create Coffee Response object', () {
+      final coffee = CoffeeResponse(file: 'coffee.jpg');
 
-    expect(coffee.file, 'coffee.jpg');
-  });
+      expect(coffee.file, 'coffee.jpg');
+    });
 
-  test(
-      'coffeeResponseFromJson should convert valid JSON string to CoffeeResponse object',
-      () {
-    String jsonString = '{"file": "https://example.com/image.jpg"}';
+    test(
+        'coffeeResponseFromJson should convert valid JSON string to CoffeeResponse object',
+        () {
+      String jsonString = '{"file": "https://example.com/image.jpg"}';
 
-    CoffeeResponse coffeeResponse = coffeeResponseFromJson(jsonString);
+      CoffeeResponse coffeeResponse = coffeeResponseFromJson(jsonString);
 
-    expect(coffeeResponse.file, 'https://example.com/image.jpg');
-  });
+      expect(coffeeResponse.file, 'https://example.com/image.jpg');
+    });
 
-  test('Should encode CoffeeResponse to JSON', () {
-    CoffeeResponse coffeeResponse =
-        CoffeeResponse(file: 'https://example.com/image.jpg');
-    String jsonResult = coffeeResponseToJson(coffeeResponse);
-    expect(jsonResult, '{"file":"https://example.com/image.jpg"}');
+    test('Should encode CoffeeResponse to JSON', () {
+      CoffeeResponse coffeeResponse =
+          CoffeeResponse(file: 'https://example.com/image.jpg');
+      String jsonResult = coffeeResponseToJson(coffeeResponse);
+      expect(jsonResult, '{"file":"https://example.com/image.jpg"}');
+    });
   });
 }
